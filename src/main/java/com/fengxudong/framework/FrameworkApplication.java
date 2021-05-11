0c77f8cfc7dbfe179e5cd0a473d31405465b3bb3
fengxudong <fengxudong@comisys.net>
2021-05-11 14:44:58 +0800

    [feature]权限注解注册器，应用启动时扫描@RoleMapping

--- src/main/java/com/fengxudong/framework/FrameworkApplication.java
+++ src/main/java/com/fengxudong/framework/FrameworkApplication.java
@@ -1,12 +1,13 @@
 package com.fengxudong.framework;
 
+import com.fengxudong.framework.init.scanner.RoleMappingScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = "com.fengxudong.framework")
 @EnableTransactionManagement
+@RoleMappingScan(baseScanPackage = "classpath:com/fengxudong/framework")
 public class FrameworkApplication {
 
     public static void main(String[] args) {
